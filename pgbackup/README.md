7d18d1a86067b8bf7e67120a67499f9f67702fc1
reynaldo <reynaldo@linux-v71x.home>
2021-11-09 17:53:28 +0100

    Created package and parser

---
+++ pgbackup/README.md
@@ -0,0 +1,4 @@
+pgbackup
+========
+
+CLI for backing up remote PostgresSQL databases locally or to AWS S3 
--- pgbackup/setup.py
+++ pgbackup/setup.py
@@ -4,6 +4,12 @@ with open('README.md','r') as f:
     long_description = f.read()
 
 setup(
-
-    
+    name='pgbackup',
+    version='0.1.0',
+    author='Reynaldo Luevano',
+    author_email='reynaldoluevano@gmail.com',
+    packages=find_packages('src'),
+    description='A utility for backing up PostgreSQL Databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
